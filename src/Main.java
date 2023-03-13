9a1977a6aa1d067fd1c43e5c4f11b5adc0382bc6
Kostya Rakhimzyanov <kroshavr@mail.ru>
2023-03-13 20:19:30 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args){
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "AAA");
+        map.put(2, "BBB");
+        map.put(3, "CCC");
 
+        Set <Map.Entry <Integer, String>> set = map.entrySet();
+
+        Iterator<Map.Entry<Integer, String>> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        for (Map.Entry<Integer, String> i : set) {
+            System.out.print(i + ", ");
+        }
     }
 }
